e39f608a8de8071677c2beb9dc618a5d850d32c6
sin2pi <sine2pi@gmail.com>
2025-06-21 15:47:51 -0700

    Update model.py

--- model.py
+++ model.py
@@ -34,21 +34,6 @@ dtype = torch.float32
 warnings.filterwarnings("ignore")
 logging.basicConfig(level=logging.ERROR)
 
-from rich.traceback import install
-install(show_locals=True)
-
-import pretty_errors
-pretty_errors.configure(
-    separator_character = '*',
-    filename_display    = pretty_errors.FILENAME_EXTENDED,
-    line_number_first   = True,
-    display_link        = True,
-    lines_before        = 5,
-    lines_after         = 2,
-    line_color          = pretty_errors.RED + '> ' + pretty_errors.default_config.line_color,
-    code_color          = '  ' + pretty_errors.default_config.line_color,
-)
-
 extractor = None
 tokenizer = None
 optimizer = None
